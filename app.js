17f4303f6dcb3b0f60cfc40f16ffe5cbc96a1b76
Aditya Mittal <mittaladitya13@gmail.com>
2014-09-03 01:34:59 -0400

    Created website

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+
+$(document).ready(function() {    
+	$('#hkust').width("100px");
+});
+
+
+	/*
+    $('.article').click(function() {
+
+  	('.article').removeClass('current');
+    $('.description').hide();
+
+    $(this).addClass('current');
+	$(this).('.description').show();
+   
+  }); */
+};
+
+
+
